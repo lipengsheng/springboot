ed8aac8129e39f4d91f6bc507ba089f447dedefc
lipengsheng <2439453924@qq.com>
2018-06-21 15:50:34 +0800

    spring boot 的测试:
       1.在pom.xml加上这个dependency依赖：
    
       <dependency>
          <groupId>org.springframework.boot</groupId>
          <artifactId>spring-boot-starter-test</artifactId>
           <scope>test</scope>
      </dependency>
       2.想要测试什么类，就在对应的test目录建立一个跟main目录结构一样的测试类
       3.然后在这个测试类上面加上这几个注解
         @RunWith(SpringRunner.class)
         @SpringBootTest即可

--- src/main/java/com/cn/shenzhen/controller/UserController.java
+++ src/main/java/com/cn/shenzhen/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.cn.shenzhen.controller;
 
 import com.cn.shenzhen.common.pojo.User;
-import com.cn.shenzhen.serviece.UserService;
+import com.cn.shenzhen.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
